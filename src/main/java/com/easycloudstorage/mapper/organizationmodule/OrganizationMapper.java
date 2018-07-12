ee5646c2050fc4a732477a237c9ced0e154bddbc
7005881Zhang <40751353+7005881Zhang@users.noreply.github.com>
2018-07-12 18:13:07 +0800

    添加了组织的创建页面以及homepage注释

--- src/main/java/com/easycloudstorage/mapper/organizationmodule/OrganizationMapper.java
+++ src/main/java/com/easycloudstorage/mapper/organizationmodule/OrganizationMapper.java
@@ -1,9 +1,21 @@
 package com.easycloudstorage.mapper.organizationmodule;
 
+import com.easycloudstorage.pojo.Directory;
+import com.easycloudstorage.pojo.File;
 import com.easycloudstorage.pojo.Organization;
+
+import java.util.List;
 
 public interface OrganizationMapper {
     Organization getByOrgId(int orgId);
 
+    List<Organization> orgList();
+
+    void addOrg(Organization org);
+
     int isMember(String accountId, int orgId);
+
+    void addDir(Directory directory);
+
+    Organization getOrgByName(String name);
 }
